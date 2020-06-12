0c1addffae919b5df3916cb4a22d8ea57fc6c582
dodgez <zacharysdodge@gmail.com>
2020-06-12 09:19:31 -0700

    Set default frame size

--- init.el
+++ init.el
@@ -305,6 +305,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (when window-system (set-frame-size (selected-frame) 150 50))
   )
 
 (defun dotspacemacs/user-config ()
