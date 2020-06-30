81f460342ab21193f88a9977a1129a96cb0b9f8d
dodgez <zacharysdodge@gmail.com>
2020-06-29 17:27:22 -0700

    Fix projectile-find-file on Windows

--- init.el
+++ init.el
@@ -321,6 +321,9 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+
+  ;; Fix windows projectile-find-file
+  (setq projectile-git-submodule-command nil)
   (global-set-key [f8] 'neotree-toggle)
   (move-text-default-bindings)
   (setq hl-todo-keyword-faces
